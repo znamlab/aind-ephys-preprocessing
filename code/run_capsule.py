a5c9327a0542972d55739e011ded09443199cbc3
Antonin Blot <antonin.blot@crick.ac.uk>
2026-01-05 17:23:38 +0000

    Write reason for skipping in error.txt
    It was hardcoded to "Too many bad channels", now write when it's
    skipped because of "Recording too short"

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -388,6 +388,7 @@ if __name__ == "__main__":
                 channel_labels = None
                 skip_processing = True
                 skip_reason = "Recording too short"
+                skip_reason = "Recording too short"
             else:
                 # IBL bad channel detection
                 _, channel_labels = spre.detect_bad_channels(
@@ -413,6 +414,7 @@ if __name__ == "__main__":
                     preprocessing_notes += f"\n- Found {len(all_bad_channel_ids)} bad channels."
                     if preprocessing_params["remove_bad_channels"]:
                         skip_processing = True
+                        skip_reason = "Too many bad channels"
                         skip_reason = "Too many bad channels"
                         logging.info("\tSkipping further processing for this recording.")
                         preprocessing_notes += f" Skipping further processing for this recording.\n"
@@ -612,7 +614,7 @@ if __name__ == "__main__":
                 # make a dummy file if too many bad channels to skip downstream processing
                 preprocessing_output_folder.mkdir()
                 error_file = preprocessing_output_folder / "error.txt"
-                error_file.write_text(skip_reason)
+                error_file.write_text(skip_reason or "Unknown skip reason")
 
             # store recording for drift visualization
             preprocessing_visualization_data[recording_name]["drift"] = dict(
